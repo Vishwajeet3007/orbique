79dedd4f21b14deb67df5e414ac4cea3c7642e81
Vishwajeet Kumar <166978248+Vishwajeet3007@users.noreply.github.com>
2025-10-10 11:18:57 +0000

    Patterns problem

---
+++ Patterns/5.py
@@ -0,0 +1,8 @@
+for i in range(30):
+    for j in range(30):
+        if i == 0 or i == 9 or j == 0 or j == 9 or i == j or i + j == 9:
+            
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print("")
